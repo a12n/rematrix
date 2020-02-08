d6e6a3081e2a765acdd7f00cddc69c934d508111
Anton Yabchinskiy <arn@bestmx.net>
2020-02-08 23:56:20 +0300

    Add header file with declaration and std:: imports

---
+++ decl.hpp
@@ -0,0 +1,25 @@
+#ifndef REMATRIX_DECL_HPP
+#define REMATRIX_DECL_HPP
+
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+namespace rematrix {
+
+namespace this_thread = std::this_thread;
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::steady_clock;
+using std::function;
+using std::make_pair;
+using std::pair;
+using std::remove_pointer_t;
+using std::runtime_error;
+using std::unique_ptr;
+
+} // namespace rematrix
+
+#endif  // REMATRIX_DECL_HPP
