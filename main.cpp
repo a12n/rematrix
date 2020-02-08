e817421bfc04f58de4beb8c2df7e57f4cea65cd2
Anton Yabchinskiy <arn@bestmx.net>
2020-02-09 00:03:32 +0300

    init() and render() functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,44 @@
+#include <GL/glew.h>
+
 #include "rendering_context.hpp"
 
 using namespace rematrix;
+
+namespace {
+
+void
+init(const pair<unsigned int, unsigned int>& window_size)
+{
+    if (auto status{glewInit()}; status != GLEW_OK) {
+        throw runtime_error(reinterpret_cast<const char*>(glewGetErrorString(status)));
+    }
+
+    if (! GLEW_VERSION_3_0) {
+        throw runtime_error("OpenGL 3.0 required");
+    }
+
+    glViewport(0, 0, window_size.first, window_size.second);
+
+    glClearColor(0, 0, 0, 1);
+}
+
+void
+render(const duration<float>& dt)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+} // namespace
 
 int
 main()
 {
     rendering_context ctx;
 
+    init(ctx.window_size());
+
     while (true) {
+        render(duration<float>{});
         ctx.swap_buffers();
     }
 
