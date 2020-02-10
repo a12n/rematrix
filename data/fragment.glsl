56b9bdb258dfd4400a3da097cb51b9ecdd97ed21
Anton Yabchinskiy <arn@bestmx.net>
2020-02-10 13:50:25 +0300

    Shader code in separate files

---
+++ data/fragment.glsl
@@ -0,0 +1,7 @@
+#version 130
+
+void
+main()
+{
+    gl_FragColor = vec4(0.1, 0.2, 0.3, 1.0);
+}
---
+++ data/vertex.glsl
@@ -0,0 +1,9 @@
+#version 130
+
+attribute vec3 position;
+
+void
+main()
+{
+    gl_Position = vec4(position, 1.0);
+}
