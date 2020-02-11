fba991a74c38f8cd081f7324194efc24f9994758
Anton Yabchinskiy <arn@bestmx.net>
2020-02-11 23:46:50 +0300

    Declare data constants in resources.hpp

---
+++ resources.hpp
@@ -0,0 +1,11 @@
+#ifndef REMATRIX_RESOURCES_HPP
+#define REMATRIX_RESOURCES_HPP
+
+namespace rematrix {
+
+extern const char vertex_src[];
+extern const char fragment_src[];
+
+} // namespace rematrix
+
+#endif  // REMATRIX_RESOURCES_HPP
