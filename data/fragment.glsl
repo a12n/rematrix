b2702b1784f3742af0c47a084a02615ab8b09cac
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 02:09:11 +0300

    data/fragment: alternative distance field handling

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -4,12 +4,13 @@ in vec2 tex_coord;
 
 uniform sampler2D tex;
 
-const float smoothing = 0.025;
+const float fill = 0.5;
+const float falloff = 0.03;
 
 void
 main()
 {
-    float dist = texture(tex, tex_coord).r;
-    float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, dist);
+    float dist = 1.0 - texture(tex, tex_coord).r;
+    float alpha = 1.0 - smoothstep(fill, fill + falloff, dist);
     gl_FragColor = vec4(1, 1, 1, alpha);
 }
