c937eda56346e39a348e10b6f00aed0a9dd64df2
Anton Yabchinskiy <arn@bestmx.net>
2020-02-12 13:30:23 +0300

    Embed font description in resources.cpp

--- resources.cpp.sh
+++ resources.cpp.sh
@@ -19,5 +19,9 @@ const unsigned char font_image_data[]{
 $(convert data/font.png -depth 8 gray:- | od -v -A n -t u1 -w1 | sed 's/^ *//;s/$/,/')
 };
 
+const font_descr font = {
+$(jq -r '. as $top | .characters | to_entries[] | "{ '\''\(.key)'\'', {{\(.value.x / $top.width), \(.value.y / $top.height)}, {\(.value.width / $top.width), \(.value.height / $top.height)}} },"' data/font.json)
+};
+
 } // namespace rematrix
 EOF
--- resources.hpp
+++ resources.hpp
@@ -1,6 +1,8 @@
 #ifndef REMATRIX_RESOURCES_HPP
 #define REMATRIX_RESOURCES_HPP
 
+#include "font_descr.hpp"
+
 namespace rematrix {
 
 extern const char vertex_src[];
@@ -9,6 +11,8 @@ extern const char fragment_src[];
 extern const unsigned short font_image_width;
 extern const unsigned short font_image_height;
 extern const unsigned char font_image_data[];
+
+extern const font_descr font;
 
 } // namespace rematrix
 
