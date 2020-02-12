8eb1d4e0a4fd22262bc1f093c743c2c719d9f694
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 02:44:57 +0300

    Add vertex_array struct

---
+++ vertex_array.hpp
@@ -0,0 +1,32 @@
+#ifndef REMATRIX_VERTEX_ARRAY_HPP
+#define REMATRIX_VERTEX_ARRAY_HPP
+
+#include <GL/glew.h>
+
+namespace rematrix {
+
+struct vertex_array
+{
+    vertex_array();
+
+    vertex_array(const vertex_array&) = delete;
+
+    vertex_array(vertex_array&& other) noexcept;
+
+    ~vertex_array();
+
+    vertex_array&
+    operator=(const vertex_array&) = delete;
+
+    vertex_array&
+    operator=(vertex_array&& other) noexcept;
+
+    void
+    bind();
+
+    const GLuint id{0};
+};
+
+} // namespace rematrix
+
+#endif  // REMATRIX_VERTEX_ARRAY_HPP
