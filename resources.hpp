f67c016a98267c1c8718cfe5b7d028839b56e534
Anton Yabchinskiy <arn@bestmx.net>
2020-02-12 13:29:51 +0300

    Embed grayscale font image in resources.cpp

--- resources.hpp
+++ resources.hpp
@@ -6,6 +6,10 @@ namespace rematrix {
 extern const char vertex_src[];
 extern const char fragment_src[];
 
+extern const unsigned short font_image_width;
+extern const unsigned short font_image_height;
+extern const unsigned char font_image_data[];
+
 } // namespace rematrix
 
 #endif  // REMATRIX_RESOURCES_HPP
