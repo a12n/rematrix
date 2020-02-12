b00e6450e1be158c4e184896603be5921225d231
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 02:14:14 +0300

    data/fragment: fill_color uniform

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -1,8 +1,12 @@
 #version 130
 
+const vec3 neutral_blue = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
+const vec3 faded_blue   = vec3(0x07 / 255.0, 0x66 / 255.0, 0x78 / 255.0);
+
 in vec2 tex_coord;
 
 uniform sampler2D tex;
+uniform vec3 fill_color = neutral_blue;
 
 const float fill = 0.5;
 const float falloff = 0.03;
@@ -12,5 +16,5 @@ main()
 {
     float dist = 1.0 - texture(tex, tex_coord).r;
     float alpha = 1.0 - smoothstep(fill, fill + falloff, dist);
-    gl_FragColor = vec4(1, 1, 1, alpha);
+    gl_FragColor = vec4(fill_color, alpha);
 }
