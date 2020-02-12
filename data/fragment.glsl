35ed08e35a0f9f76bd03d2bd76bd675b8e51037f
Anton Yabchinskiy <arn@bestmx.net>
2020-02-12 16:43:09 +0300

    data/fragment: distance field rendering

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -4,8 +4,12 @@ in vec2 tex_coord;
 
 uniform sampler2D tex;
 
+const float smoothing = 0.75 / 16.0;
+
 void
 main()
 {
-    gl_FragColor = vec4(1, 1, 1, texture(tex, tex_coord).r);
+    float dist = texture(tex, tex_coord).r;
+    float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, dist);
+    gl_FragColor = vec4(1, 1, 1, alpha);
 }
