b74fe56506ddf4af1937a31acb8274bc7d27afeb
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 15:02:50 +0300

    Use float in font_descr

--- font_descr.hpp
+++ font_descr.hpp
@@ -7,19 +7,19 @@ namespace rematrix {
 
 struct glyph_descr
 {
-    const array<double, 2> pos;
-    const array<double, 2> size;
+    const array<float, 2> pos;
+    const array<float, 2> size;
 };
 
 struct font_descr
 {
-    array<array<double, 2>, 4>
+    array<array<float, 2>, 4>
     tex_coords(char c) const
     {
         return tex_coords(glyphs.at(c));
     }
 
-    array<array<double, 2>, 4>
+    array<array<float, 2>, 4>
     tex_coords(const glyph_descr& g) const
     {
         auto [x, y] = g.pos;
