729776057241df8fef12a12ad7ae5e3364925278
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 03:48:40 +0300

    Identity matrix

--- matrix.hpp
+++ matrix.hpp
@@ -8,6 +8,13 @@ namespace rematrix {
 using vector4 = array<float, 4>;
 using matrix4 = array<vector4, 4>;
 
+inline const matrix4 identity_matrix{
+    { {1.0, 0.0, 0.0, 0.0},
+      {0.0, 1.0, 0.0, 0.0},
+      {0.0, 0.0, 1.0, 0.0},
+      {0.0, 0.0, 0.0, 1.0} }
+};
+
 } // namespace rematrix
 
 #endif  // REMATRIX_MATRIX_HPP
