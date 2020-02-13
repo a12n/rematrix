3f942e45826600310b3a9d8ee9b300ba3452b2cb
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 03:49:02 +0300

    data/vertex: uniforms for model, view and projection matrices

--- data/vertex.glsl
+++ data/vertex.glsl
@@ -5,9 +5,13 @@ in vec2 tex_coord;
 
 out vec2 tex_coord2;
 
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
+
 void
 main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(position, 0.0, 1.0);
     tex_coord2 = tex_coord;
 }
