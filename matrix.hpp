206cce43d3b7f013517f70cfc19e83ece46b9370
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 15:18:17 +0300

    Limited set of GLM headers

--- matrix.hpp
+++ matrix.hpp
@@ -1,8 +1,13 @@
 #ifndef REMATRIX_MATRIX_HPP
 #define REMATRIX_MATRIX_HPP
 
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_projection.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 namespace rematrix {
 
@@ -10,7 +15,6 @@ using glm::vec3;
 using glm::vec4;
 using glm::mat4;
 
-using glm::identity;
 using glm::perspective;
 using glm::radians;
 using glm::rotate;
