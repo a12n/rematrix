a76080df31631a2eca14d11314b7c2bf7f1fbe72
Anton Yabchinskiy <arn@bestmx.net>
2020-02-13 14:56:07 +0300

    data/fragment: simpler SDF shader

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -1,27 +1,16 @@
 #version 130
 
-const vec3 neutral_blue = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
-const vec3 faded_blue   = vec3(0x07 / 255.0, 0x66 / 255.0, 0x78 / 255.0);
-
 in vec2 texCoord2;
 
 uniform sampler2D sdfTex;
-uniform vec3 fill_color = neutral_blue;
-uniform vec3 fill_color2 = faded_blue;
+uniform vec3 fillColor = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
 
-const float fill = 0.5;
-const float falloff = 0.03;
-
-const float fill2 = 0.2;
-const float falloff2 = 0.8;
+const float smoothing = 0.001;
 
 void
 main()
 {
-    float dist = 1.0 - texture(sdfTex, texCoord2).r;
-    float alpha = 1.0 - smoothstep(fill, fill + falloff, dist);
-    float alpha2 = 1.0 - smoothstep(fill2, fill2 + falloff2, dist);
-    float alpha_ans = alpha + (1.0 - alpha) * alpha2;
-    vec3 color_ans = mix(fill_color2, fill_color, alpha / alpha_ans);
-    gl_FragColor = vec4(color_ans, alpha_ans);
+    float d = texture(sdfTex, texCoord2).r;
+    float a = smoothstep(0.5 - smoothing, 0.5 + smoothing, d);
+    gl_FragColor = vec4(fillColor, a);
 }
