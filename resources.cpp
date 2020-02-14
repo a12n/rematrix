0340556d55d9dd137619038228ea15c7d27d9e70
Anton Yabchinskiy <arn@bestmx.net>
2020-02-14 10:48:53 +0300

    Add resources.cpp which #includes generated resource representations

---
+++ resources.cpp
@@ -0,0 +1,24 @@
+#include "resources.hpp"
+
+namespace rematrix {
+
+const char vertex_src[] =
+#include "vertex_src.inc"
+    ;
+
+const char fragment_src[] =
+#include "fragment_src.inc"
+    ;
+
+const array<unsigned short, 2> font_image_size{
+#include "font_image_size.inc"
+};
+const unsigned char font_image_data[]{
+#include "font_image_data.inc"
+};
+
+const font_descr font{
+#include "font.inc"
+};
+
+} // namespace rematrix
