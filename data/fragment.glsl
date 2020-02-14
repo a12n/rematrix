2dc5aadeac16cadfd772d7667cc46df3de0d74cf
Anton Yabchinskiy <arn@bestmx.net>
2020-02-14 17:34:44 +0300

    data/fragment: alternative fragment shader

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -5,12 +5,13 @@ in vec2 texCoord2;
 uniform sampler2D sdfTex;
 uniform vec3 fillColor = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
 
-const float smoothing = 0.001;
-
 void
 main()
 {
-    float d = texture(sdfTex, texCoord2).r;
-    float a = smoothstep(0.5 - smoothing, 0.5 + smoothing, d);
-    gl_FragColor = vec4(fillColor, a);
+    float s = texture(sdfTex, texCoord2).r;
+    float c = 1.0 / fwidth(s);
+    float d = (s - 0.5) * c;
+    float alpha = clamp(d + 0.5, 0.0, 1.0);
+    // gl_FragColor = vec4(fillColor, min(alpha + 0.5, 1.0));
+    gl_FragColor = vec4(fillColor, alpha);
 }
