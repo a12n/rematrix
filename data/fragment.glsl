05623e4c2e531783146fce275a03cd50b2d7f6fd
Anton Yabchinskiy <arn@bestmx.net>
2020-02-15 03:28:57 +0300

    data/fragment: different color for feeder characters

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -5,6 +5,9 @@ in float fragDepth;
 
 uniform sampler2D sdfTex;
 uniform vec3 fillColor = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
+uniform bool feederGlyph = false;
+
+const vec3 feederColor = vec3(0x83 / 255.0, 0xa5 / 255.0, 0x98 / 255.0);
 
 void
 main()
@@ -14,5 +17,5 @@ main()
     float d = (s - 0.5) * c;
     float alpha = clamp(d + 0.5, 0.0, 1.0);
     // gl_FragColor = vec4(fillColor, min(alpha + 0.5, 1.0));
-    gl_FragColor = vec4(fillColor, smoothstep(24, 23, fragDepth) * alpha);
+    gl_FragColor = vec4(feederGlyph ? feederColor : fillColor, smoothstep(24, 23, fragDepth) * alpha);
 }
