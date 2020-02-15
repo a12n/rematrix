79b30ad8f8594c9cd90b055f156709175adb8f66
Anton Yabchinskiy <arn@bestmx.net>
2020-02-15 03:17:29 +0300

    data/vertex: pass vertex depth to fragment shader

--- data/vertex.glsl
+++ data/vertex.glsl
@@ -4,6 +4,7 @@ in vec2 position;
 in vec2 texCoord;
 
 out vec2 fragTexCoord;
+out float fragDepth;
 
 uniform mat4 model = mat4(1.0);
 uniform mat4 projection = mat4(1.0);
@@ -12,6 +13,8 @@ uniform mat4 view = mat4(1.0);
 void
 main()
 {
-    gl_Position = projection * view * model * vec4(position, 0.0, 1.0);
+    vec4 ans = model * vec4(position, 0.0, 1.0);
+    gl_Position = projection * view * ans;
     fragTexCoord = texCoord;
+    fragDepth = ans.z;
 }
