9ee94568d09355db24df330f4b289869f28acd02
Anton Yabchinskiy <arn@bestmx.net>
2020-02-15 03:18:04 +0300

    data/fragment: hide strips which are too close to the camera

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -1,6 +1,7 @@
 #version 130
 
 in vec2 fragTexCoord;
+in float fragDepth;
 
 uniform sampler2D sdfTex;
 uniform vec3 fillColor = vec3(0x45 / 255.0, 0x85 / 255.0, 0x88 / 255.0);
@@ -13,5 +14,5 @@ main()
     float d = (s - 0.5) * c;
     float alpha = clamp(d + 0.5, 0.0, 1.0);
     // gl_FragColor = vec4(fillColor, min(alpha + 0.5, 1.0));
-    gl_FragColor = vec4(fillColor, alpha);
+    gl_FragColor = vec4(fillColor, smoothstep(24, 23, fragDepth) * alpha);
 }
