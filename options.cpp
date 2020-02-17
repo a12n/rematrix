b39df2ff82ec0ef035c5e8c20e5ff8fafb90a3f3
Anton Yabchinskiy <arn@bestmx.net>
2020-02-17 16:15:37 +0300

    options: use getopt

--- options.cpp
+++ options.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+
+#include <unistd.h>
+
+#include "decl.hpp"
 #include "options.hpp"
 
 namespace rematrix {
@@ -5,9 +10,21 @@ namespace rematrix {
 options
 parse_options(int argc, char* argv[])
 {
+    using std::cerr;
+    using std::endl;
+
     options ans;
 
-    // TODO
+    int opt{-1};
+    while ((opt = getopt(argc, argv, "")) != -1) {
+        switch (opt) {
+        default :
+            cerr << "Usage: " << argv[0]
+                 << endl;
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
 
     return ans;
 }
