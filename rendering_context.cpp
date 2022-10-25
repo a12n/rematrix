8f5966ff77415d02e6e8b035b75d6ba4ad3bc59f
Anton Yabchinskiy <arn@bestmx.net>
2022-10-26 01:17:02 +0300

    rendering_context: format with clang-format

--- rendering_context.cpp
+++ rendering_context.cpp
@@ -2,68 +2,61 @@
 
 namespace rematrix {
 
-rendering_context::rendering_context() :
-    display{open_display()},
-    window{XRootWindow(display.get(), XDefaultScreen(display.get()))},
-    context{create_context(choose_fb_config(XDefaultScreen(display.get())))},
-    obscured{false}
+rendering_context::rendering_context()
+    : display { open_display() }
+    , window { XRootWindow(display.get(), XDefaultScreen(display.get())) }
+    , context { create_context(choose_fb_config(XDefaultScreen(display.get()))) }
 {
-    if (! glXMakeCurrent(display.get(), window, context.get())) {
+    if (!glXMakeCurrent(display.get(), window, context.get())) {
         throw runtime_error("couldn't activate rendering context");
     }
     // XSelectInput(display.get(), window, VisibilityChangeMask);
     // XSync(display.get(), False);
 }
 
-void
-rendering_context::swap_buffers()
+void rendering_context::swap_buffers()
 {
     glXSwapBuffers(display.get(), window);
     // update_visibility();
 }
 
-array<unsigned int, 2>
-rendering_context::window_size() const
+array<unsigned int, 2> rendering_context::window_size() const
 {
     Window root;
     int x, y;
     unsigned int width, height;
     unsigned int border_width, depth;
 
-    if (! XGetGeometry(display.get(), window,
-                       &root,
-                       &x, &y,
-                       &width, &height,
-                       &border_width,
-                       &depth))
-    {
+    if (!XGetGeometry(display.get(), window,
+            &root,
+            &x, &y,
+            &width, &height,
+            &border_width,
+            &depth)) {
         throw runtime_error("couldn't get window size");
     }
 
-    return {{width, height}};
+    return { { width, height } };
 }
 
-bool
-rendering_context::window_obscured() const
+bool rendering_context::window_obscured() const
 {
     // const_cast<rendering_context*>(this)->update_visibility();
     return obscured;
 }
 
-rendering_context::display_ptr
-rendering_context::open_display()
+rendering_context::display_ptr rendering_context::open_display()
 {
-    display_ptr ans{XOpenDisplay(nullptr), XCloseDisplay};
+    display_ptr ans { XOpenDisplay(nullptr), XCloseDisplay };
 
-    if (! ans) {
+    if (!ans) {
         throw runtime_error("couldn't open display");
     }
 
     return ans;
 }
 
-GLXFBConfig
-rendering_context::choose_fb_config(int screen) const
+GLXFBConfig rendering_context::choose_fb_config(int screen) const
 {
     const int attrs[] = {
         GLX_RED_SIZE, 5,
@@ -73,42 +66,40 @@ rendering_context::choose_fb_config(int screen) const
         None
     };
 
-    int n{0};
-    unique_ptr<GLXFBConfig, decltype(&XFree)> configs{glXChooseFBConfig(display.get(), screen, attrs, &n), XFree};
-    if (! configs || n < 1) {
+    int n { 0 };
+    unique_ptr<GLXFBConfig, decltype(&XFree)> configs { glXChooseFBConfig(display.get(), screen, attrs, &n), XFree };
+    if (!configs || n < 1) {
         throw runtime_error("couldn't choose frame buffer configuration");
     }
 
     return configs.get()[0];
 }
 
-rendering_context::context_ptr
-rendering_context::create_context(GLXFBConfig config) const
+rendering_context::context_ptr rendering_context::create_context(GLXFBConfig config) const
 {
     auto context_deleter =
-        [this] (GLXContext c) -> void {
-            glXDestroyContext(display.get(), c);
-        };
-    context_ptr ans{glXCreateNewContext(display.get(), config, GLX_RGBA_TYPE, nullptr, True), context_deleter};
+        [this](GLXContext c) -> void {
+        glXDestroyContext(display.get(), c);
+    };
+    context_ptr ans { glXCreateNewContext(display.get(), config, GLX_RGBA_TYPE, nullptr, True), context_deleter };
 
-    if (! ans) {
+    if (!ans) {
         throw runtime_error("couldn't create rendering context");
     }
 
     return ans;
 }
 
-void
-rendering_context::update_visibility()
+void rendering_context::update_visibility()
 {
     while (XPending(display.get()) > 0) {
         XEvent ev;
         XNextEvent(display.get(), &ev);
         switch (ev.type) {
-        case VisibilityNotify :
+        case VisibilityNotify:
             obscured = (ev.xvisibility.state == VisibilityFullyObscured);
             break;
-        default :
+        default:
             break;
         };
     }
